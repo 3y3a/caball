fd840e661b74b1f575f11a918c1c170ce66d978c
3y3a <65492089+3y3a@users.noreply.github.com>
2020-05-17 16:47:36 +0500

    Add files via upload

---
+++ CABAL.py
@@ -0,0 +1,261 @@
+import discord 
+import math
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.ext.commands import MemberConverter
+from discord.utils import get
+from datetime import datetime, timedelta
+from datetime import date
+from time import sleep
+import os
+import requests
+from PIL import Image, ImageFont, ImageDraw
+import io
+import time
+from random import randint
+import sqlite3
+
+conn = sqlite3.connect('Chinook_Sqlite.sqlite')
+cursor = conn.cursor()
+
+token = open( 'token.txt', 'r').readline()
+
+key_words = ["CABAL", "cabal", "Cabal"]
+
+cabal = commands.Bot(command_prefix= "!")
+
+@cabal.event                                
+async def on_connect():                                       #Ok
+    print("Welcome back Commander")
+    #channelup = cabal.get_channel(695286021176426536)
+    #await channelup.send("``Подключён``")
+    await cabal.change_presence( status = discord.Status.online, activity = discord.CustomActivity ("123"))
+    #activity = discord.Game ("C&C")) 
+
+@cabal.command(pass_context= True)  
+async def пнул(ctx, user: discord.User):
+    await ctx.channel.purge(limit = 2)
+    await ctx.send(f"{user.mention}")
+
+@cabal.command(pass_context= True)           
+async def Справка(ctx):
+    author = ctx.message.author
+    await ctx.send(f" {author.mention} Приветствую, я #дополнить# ")
+
+@cabal.command(pass_context= True)                          #Ok
+async def Время(ctx):
+    await ctx.send(f"```Подключаю модуль времени...```")
+    time = datetime.now()
+    mtime = timedelta(hours=2)
+    mtime = time - mtime
+    mtime = mtime.strftime("%H:%M")
+    await ctx.send(f" ```Текущее время МСК {mtime} ```")
+    
+@cabal.command(pass_context= True)
+async def Новый_период(ctx):
+    time = date.today()
+    timenext = timedelta(days = 2)
+    timenext = time + timenext
+    time = time.strftime("%d/%m")
+    timenext = timenext.strftime("%d/%m")
+    cursor.execute(f"UPDATE Legates SET datenow = ('{time}'), datenext = ('{timenext}')")
+    conn.commit()
+
+cursor.execute("SELECT datenow, datenext FROM Legates")
+datedoklad = cursor.fetchall()
+datenow = datedoklad[0][0]
+datenext = datedoklad[0][1] 
+
+@cabal.command(pass_context= True)                          
+async def Очистка(ctx, amount = 18):
+    await ctx.channel.purge(limit = amount)
+
+@cabal.command(pass_context= True)                          
+async def Доклад(ctx):
+    one = randint(0, 50)
+    two = randint(50, 100)
+    await ctx.send(f"Подготавливаю доклад...")
+    time.sleep(2)
+    await ctx.send(f"Чтение данных {one}%...")
+    time.sleep(2)
+    await ctx.send(f"Запрос личных данных {two}%...")
+    time.sleep(2)
+    await ctx.send(f"Готово.")
+    cursor.execute("SELECT name, time, norma FROM Legates")
+    results = cursor.fetchall()
+    await ctx.send(f"***Период: {datenow} - {datenext}***")
+    for i in range (len(results)):
+        if {results[i][2]} == {results[i][1]}:
+            await ctx.send(f"```{results[i][0]} - {results[i][1]} / {results[i][2]} (норма выполнена)```")
+        else:
+            await ctx.send(f"```{results[i][0]} - {results[i][1]} / {results[i][2]}```")
+
+@cabal.command(pass_context= True)                          #ok
+async def Запись(ctx, time):
+    cursor.execute(f"UPDATE Legates SET time = ('{time}') WHERE id = ('{ctx.author.id}')")
+    #time = cursor.execute(f"SELECT time FROM Legates")
+    conn.commit()
+    #await ctx.send(f"{ctx.author.id}")
+    #cursor.execute(f"insert into Legates values (Null, {legat}, Null, Null) ")
+    #conn.commit()
+
+
+@cabal.command(pass_context= True)
+async def Правила(ctx):
+    await ctx.send (f"""```Выбор категории:
+    SCP
+    Классы_людей```
+    """)
+
+@cabal.command(pass_context= True)
+async def SCP(ctx):
+    await ctx.send(f"""```
+    SCP_all
+    SCP_049
+    SCP_079
+    SCP_096
+    SCP_173
+    SCP_106
+    SCP_939```""")
+
+@cabal.command(pass_context= True)
+async def Классы_людей (ctx):
+    await ctx.send(f"""```
+    Human_all
+    Dboi
+    Scientist
+    Security
+    MTF
+    CR```""")
+
+@cabal.command(pass_context= True)
+async def SCP_all (ctx):
+    r_all = open ("Общие_SCP.txt")
+    for line in r_all:
+        await ctx.send (line)
+    r_all.close
+
+@cabal.command(pass_context= True)
+async def SCP_049 (ctx):
+    r_049 = open ("049.txt")
+    for line in r_049:
+        await ctx.send (line)
+    r_049.close
+
+@cabal.command(pass_context= True)
+async def SCP_079 (ctx):
+    r_079 = open ("079.txt")
+    for line in r_079:
+        await ctx.send (line)
+    r_079.close
+
+@cabal.command(pass_context= True)
+async def SCP_096 (ctx):
+    r_096 = open ("096.txt")
+    for line in r_096:
+        await ctx.send (line)
+    r_096.close
+
+@cabal.command(pass_context= True)
+async def SCP_173 (ctx):
+    r_173 = open ("173.txt")
+    for line in r_173:
+        await ctx.send (line)
+    r_173.close
+
+@cabal.command(pass_context= True)
+async def SCP_939 (ctx):
+    r_939 = open ("079.txt")
+    for line in r_939:
+        await ctx.send (line)
+    r_939.close
+
+@cabal.command(pass_context= True)
+async def Human_all (ctx):
+    rh_all = open ("Общие_Люди.txt")
+    for line in rh_all:
+        await ctx.send (line)
+    rh_all.close
+
+@cabal.command(pass_context= True)
+async def Dboi (ctx):
+    r_d = open ("D.txt")
+    for line in r_d:
+        await ctx.send (line)
+    r_d.close
+
+@cabal.command(pass_context= True)
+async def Scientist (ctx):
+    r_s = open ("Science.txt")
+    for line in r_s:
+        await ctx.send (line)
+    r_s.close
+
+@cabal.command(pass_context= True)
+async def Security (ctx):
+    r_sec = open ("Security.txt")
+    for line in r_sec:
+        await ctx.send (line)
+    r_sec.close
+
+@cabal.command(pass_context= True)
+async def MTF (ctx):
+    r_mtf = open ("MTF.txt")
+    for line in r_mtf:
+        await ctx.send (line)
+    r_mtf.close
+
+@cabal.command(pass_context= True)
+async def RC (ctx):
+    r_ph = open ("PH.txt")
+    for line in r_ph:
+        await ctx.send (line)
+    r_ph.close
+
+
+
+@cabal.command(pass_context= True)
+async def Досье(ctx, user: discord.User):                   #Ok
+       
+    img = Image.new("RGBA", (400,200), "#808080" )
+    url = str(user.avatar_url) [:-10]
+
+    response = requests.get(url, stream = True)
+    response = Image.open(io.BytesIO(response.content))
+    response = response.convert("RGBA")
+    response = response.resize((100, 100), Image.ANTIALIAS)
+
+    img.paste(response, (15, 15, 115, 115))
+
+    idraw = ImageDraw.Draw(img)
+    name = user.name
+    tag = user.id
+    time = user.created_at.strftime("%Y/%m/%d %H:%M)")
+
+    headline = ImageFont.truetype("arial.ttf", size = 20)
+    undertext = ImageFont.truetype("arial.ttf", size = 12)
+            
+    idraw.text((145, 15), f"{name}", font = headline)
+    idraw.text((145, 50), f"ID: {tag}", font = undertext)
+    idraw.text((145, 70), f"Существует с:{time}", font = undertext) 
+
+    img.save("user_card.png")
+
+    await ctx.send(file = discord.File(fp = "user_card.png"))
+
+@cabal.event
+async def on_message (message):                                         #???
+    await cabal.process_commands (message)
+    mess = message.content.lower()
+    if mess in key_words:
+        #await message.delete()
+        #time.sleep (3)
+        await message.author.send(f" ```анализ упоминания...```")
+        time.sleep (4)
+        await message.author.send(f" ```{message.author.name} - здравствуйте легат, доступ разрешён .```")
+    #await message.author.send({message.author.id})
+
+
+
+
+cabal.run( token )
