8bfa5e796001e24d5206a1e339ce5c88144c474b
3y3a <65492089+3y3a@users.noreply.github.com>
2020-06-01 18:58:03 +0500

    Update CABAL.py

--- CABAL.py
+++ CABAL.py
@@ -49,72 +49,22 @@ async def Время(ctx):
     await ctx.send(f" ```Текущее время МСК {mtime} ```")
     
 @cabal.command(pass_context= True)
-async def Новый_период(ctx):
-    time = date.today()
-    timenext = timedelta(days = 2)
-    timenext = time + timenext
-    time = time.strftime("%d/%m")
-    timenext = timenext.strftime("%d/%m")
-    cursor.execute(f"UPDATE Legates SET datenow = ('{time}'), datenext = ('{timenext}')")
-    cursor.execute(f"UPDATE Legates SET time = Null, endtime = ('0:00')")
-    conn.commit()
+async def работать (ctx, user: discord.User):   
+    if ctx.message.author.id == 370199534183120897:                                      
+        await user.send(f"Ебошь блять!")
+    else:
+        await ctx.send(f"Доступ ограничен")
+    
 
-@cabal.command(pass_context= True)
-async def Зашел(ctx, server, starttime):
-    time1 = datetime.strptime(starttime,"%H:%M")
-    time1 = time1.strftime("%H:%M")
-    cursor.execute(f"UPDATE Legates SET starttime = ('{time1}') WHERE id = ('{ctx.author.id}')")
-    conn.commit()
-    await ctx.send(f"Запуск учёта времени на посту для {ctx.author.name}.")
-
-@cabal.command(pass_context= True)
-async def Вышел(ctx, server, endtime):
-    cursor.execute(f"SELECT starttime, endtime FROM Legates WHERE id = ('{ctx.author.id}')  ")
-    timeinserver = cursor.fetchall()
-    time2 = datetime.strptime(endtime,"%H:%M")
-    #time2 = time2.strftime("%H:%M")
-    time3 = timeinserver[0][0]
-    time4 = datetime.strptime(time3,"%H:%M")
-    time5 = timeinserver[0][1]
-    time6 = datetime.strptime(time5,"%H:%M")
-    #time4 = time4.strftime("%H:%M")
-    timeall = time2 - time4 + time6
-    timeall = timeall.strftime("%H:%M")
-    cursor.execute(f"UPDATE Legates SET time = ('{timeall}'), endtime = ('{timeall}') WHERE id = ('{ctx.author.id}')")
-    conn.commit()
-    await ctx.send(f"{ctx.author.name} общее время на посту: {timeall}")
-
-@cabal.command(pass_context= True)
-async def Дозапись(ctx, legat, endtime):
-    cursor.execute(f"UPDATE Legates SET time = ('{endtime}'), endtime = ('{endtime}') WHERE name = ('{legat}')")
-    conn.commit()
-    await ctx.send(f"Дозапись Легату {legat} в размере {endtime} сделана.")
-@cabal.command(pass_context= True)                          
-async def Доклад(ctx):
-    one = randint(0, 50)
-    two = randint(50, 100)
-    await ctx.send(f"Подготавливаю доклад...")
-    time.sleep(1)
-    await ctx.send(f"Чтение данных {one}%...")
-    time.sleep(1)
-    await ctx.send(f"Запрос личных данных {two}%...")
-    time.sleep(1)
-    await ctx.send(f"Готово.")
-    cursor.execute("SELECT name, time, norma, datenow, datenext FROM Legates")
-    results = cursor.fetchall()
-    await ctx.send(f"***Период: {results[0][3]} - {results[0][4]}***")
-    f = open ("test.txt", "w")
-    for i in range (len(results)):
-        if {results[i][2]} == {results[i][1]}:
-            #await ctx.send(f"```{results[i][0]} - {results[i][1]} / {results[i][2]} (норма выполнена)```")
-            f.write(f"{results[i][0]} - {results[i][1]} / {results[i][2]} (норма выполнена) \n \n")
-        else:
-            #await ctx.send(f"```{results[i][0]} - {results[i][1]} / {results[i][2]}```")
-            f.write(f"{results[i][0]} - {results[i][1]} / {results[i][2]} \n \n")
-    f.close()
-    f = open ("test.txt", "r")
-    await ctx.send(f"```{f.read()}```")
-
+@cabal.command(pass_context= True)  
+async def заеби(ctx, user: discord.User):
+    #author = ctx.message.author.id
+    if ctx.message.author.id == 370199534183120897:
+        for i in range (100):
+            await ctx.channel.purge(limit = 2)
+            await ctx.send(f"{user.mention}")
+    else:
+        await ctx.send(f"Доступ ограничен")
 
 @cabal.event
 async def on_message (message):                                         
